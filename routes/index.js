a2c582c77e3b32449a84f6d8a4905fd68c29ab4b
Anže Jenšterle <anze@anze.xyz>
2020-07-09 13:51:05 +0200

    some restructuring

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,6 @@ const { Router } = require("express");
 let router = new Router();
 
 router.use(require('./auth'));
-router.use('/app', require('./app'))
+router.use('/app', require('./dhcp'))
 
 module.exports = router;
